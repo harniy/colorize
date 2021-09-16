144ae8240bc025ec7ecdf185c34d129cc9f835fd
harniy <forbedlam@gmail.com>
2021-09-16 23:30:14 +0300

    2

--- client/src/components/CreateColors.js
+++ client/src/components/CreateColors.js
@@ -39,7 +39,7 @@ export default function CreateColor({ colorFromApp }) {
 
   function savedColor() {
     const color = { user: userName, colors: Object.values(userColor) };
-    fetch("http://localhost:3300/save", {
+    fetch("http://localhost:4000/save", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
